d560c24c27d4e8e7c37215a1eb854c0225a97f6d
bhavesh27 <bhaveshkumar19856@gmail.com>
2019-10-31 23:31:50 +0530

    flask_restplus added

--- app.py
+++ app.py
@@ -2,18 +2,23 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pandas as pd
 import pickle
+from flask_restplus import Api
 
 app = Flask(__name__)
+api = Api(app)
+
 model = pickle.load(open('model.pkl', 'rb'))
 
 
-@app.route('/dataset',methods=['GET'])
+@api.route('/dataset',methods=['GET'])
+@api.doc()
 def get_dataset():
     dataset = pd.read_csv('sales.csv').to_json()
     return jsonify(dataset)
 
 
-@app.route('/results',methods=['POST'])
+@api.route('/results',methods=['POST'])
+@api.doc()
 def results():
 
     data = request.get_json(force=True)
